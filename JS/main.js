8bf5884d6fa63536faa05d8c89edcf3fb4bafa26
imnxtamess <davide.morisco.work@gmail.com>
2025-02-28 12:12:13 +0100

    add comments and wrap code into functions

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,7 @@
 
 const cardContainerEl = document.querySelector(".row")
 
-// make an ajax request to the boolean endpoint 
+// make an ajax request to the boolean endpoint and render the cards on page
 
 const booleanEndPoint = "https://lanciweb.github.io/demo/api/pictures/"
 
@@ -12,12 +12,29 @@ fetch(booleanEndPoint)
   .then(response => response.json())
   .then(data => {
     // cycle through the data and save the needed variables to fill the cards
-    data.forEach(thisObject => {
-      const title = thisObject.title
-      const date = thisObject.date
-      const imgUrl = thisObject.url
+    renderCards(data)
+  })
 
-      const markup = ` <div class="col d-flex justify-content-center">
+
+
+
+
+
+
+
+
+// FUNCTIONS
+
+
+/**
+ * 
+ * @param {object value} title 
+ * @param {object value} date 
+ * @param {object value} imgUrl 
+ * @returns {string} - The markup for the HTML Cards
+ */
+function markup(title, date, imgUrl) {
+  return ` <div class="col d-flex justify-content-center">
                         <div class="photo_card">
                           <div class="card_body">
                             <img class="pin" src="./assets/img/pin.svg" alt="" />
@@ -32,13 +49,21 @@ fetch(booleanEndPoint)
                           </div>
                         </div>
                       </div>`
-      console.log(markup);
-      cardContainerEl.insertAdjacentHTML('beforeend', markup)
+}
 
+/**
+ * 
+ * @param {Object Array} data 
+ * forEach Object in the given array it saves title,date,imgUrl Key values into variables
+ * and pushesh them to the html by calling the markup () function
+ * 
+ */
+function renderCards(data) {
+  data.forEach(thisObject => {
+    const title = thisObject.title
+    const date = thisObject.date
+    const imgUrl = thisObject.url
 
-    });
-
-
+    cardContainerEl.insertAdjacentHTML('beforeend', markup(title, date, imgUrl))
   })
-
-// title date imgUrl
+}
